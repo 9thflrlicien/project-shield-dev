1bab4022691542f31dbe058bfbf1247707a4024e
Apple <apple@wenyen-2.local>
2017-07-27 17:50:49 +0800

    up

--- public/javascripts/message_autoreply.js
+++ public/javascripts/message_autoreply.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
   $(document).on('click', '.tablinks' , clickMsg);
   $(document).on('click', '.addTopics', addTopics);
   $(document).on('click', '#modal-submit', modalSubmit); //新增
+  $(document).on('click', '#viewBtn', loadView);
+  $(document).on('click', '#editBtn', openEdit); //打開編輯modal
+  $(document).on('click', '#edit-submit', modalEdit);
+
 
 
     if(window.location.pathname === '/message_autoreply'){
@@ -20,13 +24,14 @@ $(document).ready(function() {
 
 
 });
+
   function modalSubmit() {
   let d = Date.now()
   let name = $('#modal-task-name').val();
   let datetime = $('#datetime').val();
-  let text = $('#modal-text').val();
+  let textInput = $('#modal-text').val();
 
-  writeUserData(auth.currentUser.uid, name, datetime, text, auth.currentUser.email.toString());
+  writeUserData(auth.currentUser.uid, name, datetime, textInput, auth.currentUser.email.toString());
 
   //塞入資料庫並重整
   $('#quickAdd').modal('hide');
@@ -40,13 +45,17 @@ $(document).ready(function() {
 }
 
 
-  function writeUserData(userId, name, text) {
+  function writeUserData(userId, name, textInput) {
   database.ref('message-autoreply/' + userId).push({
     taskName: name,
     // datetime: datetime,
-    taskText: text,
+    taskText: textInput,
     owner: auth.currentUser.email,
+ 
+
   });
+    console.log('this is textInput: '+textInput);
+
 }
 
  
@@ -84,27 +93,106 @@ $(document).ready(function() {
 
         for(var i=0;i < myIds.length;i++){
           dataArray.push(snap.child(myIds[i]).val());
-
             $('#autoreply-list').append(
               '<tr>' +
                 '<td id="' + myIds[i] + '" hidden>' + myIds[i] + '</td>' +
                 '<td>' + 'Open' + '</td>' +
                 '<td>' + 'No Setup' + '</td>' +
-                '<td><a href="#">' + dataArray[i].taskName + '</a></td>' +
+                '<td><a href="#"><b>' + dataArray[i].taskName + '</b></a></td>' +
                 '<td>' + 'Not Assigned' + '</td>' +
                 '<td>' + dataArray[i].taskText + '</td>' +
                 '<td>' +
-                '<a href="#" id="editBtn" data-toggle="modal" data-target="#editModal">Edit</a>' +
+                '<a href="#" id="editBtn" data-toggle="modal" data-target="#editModal"><b>Edit</b></a>' +
                 ' ' +
-                '<a href="#" id="viewBtn" data-toggle="modal" data-target="#viewModal">View</a>' +
+                '<a href="#" id="viewBtn" data-toggle="modal" data-target="#viewModal"><b>View</b></a>' +
                 ' ' +
-                '<a href="#" id="deleBtn">Delete</a>' +
+                '<a href="#" id="deleBtn"><b>Delete</b></a>' +
                 '</td>' +
               '</tr>'
         );
       }
     });
 }
+
+function loadView() {
+
+  $('#view-title').text(''); //標題
+  $('#view-textinput').text(''); //任務內容
+  $('#view-owne').text(''); //負責人
+
+  let key = $(this).parent().parent().find('td:first').text();
+  console.log(key);
+  let userId = auth.currentUser.uid;
+
+  database.ref('message-autoreply/' + userId + '/' + (key)).on('value', snap => {
+    let testVal = snap.val();
+    console.log(testVal);
+    // 重複出現值 要抓出來
+    $('#view-id').append(key); //編號
+    $('#view-title').append(testVal.taskName)
+    $('#view-textinput').append(testVal.taskContent); //任務內容
+    $('#view-owne').append(testVal.owner); //負責人
+  
+
+  });
+
+}
+
+function openEdit() {
+  $('#edit-taskTitle').val(''); //狀態
+  $('#edit-taskContent').val(''); //任務內容
+  $('#edit-owner').val(''); //負責人
+
+
+  let key = $(this).parent().parent().find('td:first').text();
+  let userId = auth.currentUser.uid;
+
+  database.ref('message-autoreply/' + userId + '/' + key).on('value', snap => {
+    let testVal = snap.val();
+    // console.log(testVal);
+
+    $('#edit-id').append(key);
+    $('#edit-taskTitle').val(testVal.taskName); //狀態
+    $('#edit-taskContent').val(testVal.taskContent); //任務內容
+    $('#edit-owner').val(testVal.owner); //負責人
+    // console.log(sublist);
+
+  });
+}//end open edit
+
+function modalEdit() {
+  let key = $('#edit-id').text();
+  let userId = auth.currentUser.uid;
+  var title = $('#edit-taskTitle').val(); //狀態
+  var name = $('#edit-taskContent').val(); //任務內容
+  var owne = $('#edit-owner').val(); //負責人
+  //日期
+  let d = Date.now();
+  let date = new Date(d);
+
+  // console.log(key, userId, name, cate, stat, prio, owne, desc, subt, inir, inid, auth.currentUser.email, date);
+
+  saveUserData(key, userId, name, title, owne, auth.currentUser.email, date.toString());
+
+  $('#edit-id').text(''); //
+  $('#edit-taskContent').val(''); //任務內容
+  $('#edit-taskTitle').val(''); //狀態
+  $('#edit-owner').val(''); //負責人
+
+
+  loadAutoReply();
+  $('#editModal').modal('hide');
+}//end modal edit
+
+
+function saveUserData(key, userId, name, title, owne) {
+  database.ref('message-autoreply/' + userId + '/' + key).set({
+    taskContent: name,
+    taskTitle: title,
+    owner: owne
+  });
+}
+
 
 function logout(){
   auth.signOut()
