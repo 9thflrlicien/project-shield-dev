6d8718d90c2325d2f8879e811427cb7e893e0286
Porkchop <103703029@nccu.edu.tw>
2017-08-28 17:21:54 +0800

    問卷分析

--- public/javascripts/analyzeResponse.js
+++ public/javascripts/analyzeResponse.js
@@ -2,20 +2,26 @@ $(document).on('ready', function(){
   console.log("response ready");
 });
 
-function getChart( str ) {
+function getChart( str, option ) {
   $.ajax({
     url: "/analyzeResponse",
     type: "POST",
     dateType: 'html',
     data: {
-       chart: str
+       chart: str,
+       option: 4
      },
     success: function(data) {
-      console.log(data);
+      // console.log(data);
       // $(document).html(data);
       $("#content").empty();
-      if( str=="詳細資料" ) drawChart_4(data);
-      else console.log("success error 2");
+      if( str=="某團各客戶對各工人評價" ) drawChart_1(data);
+      else if( str=="所有旅行團對該職業工人評價" ) drawChart_2(data);
+      else if( str=="詳細資料" ) drawChart_4(data);
+      else if( str=="設定" ) drawChart_5(data);
+      else {
+        console.log("success error 2");
+      }
     },
     error: function(xhr, ajaxOptions, thrownError) {
       console.log("ERRR");
@@ -29,11 +35,100 @@ function getChart( str ) {
   });
 }
 
+function drawChart_1(input) {
+  console.log(input);
+  let labels = input.group_with_workers.map(function(ele) {
+    return ele.worker_name;
+  });
+  let users_votes = [];
+  input.users_votes.map( function(ele) {
+    let obj = {
+      label: ele.vote_voter,
+      data: ele.data.map( function(e) {
+        return e.vote_score;
+      }),
+      backgroundColor: getRandomColor()
+    };
+    users_votes.push(obj);
+  });
+
+  // $('#content').css('width', 'auto').show().siblings().hide();
+  $('#content').append('<canvas id="myChart"></canvas>');
+  let ctx = $('#myChart').get(0).getContext('2d');
+  var myChart = new Chart(ctx,
+    {
+      type: 'bar',
+      data: {
+          labels: labels,
+          //group_worker order by worker_id
+          datasets: users_votes
+      },
+      options: {
+        scales: {
+           yAxes: [{
+             scaleLabel: {
+               display: true,
+               labelString: '評價分數'
+             },
+             ticks: {
+               beginAtZero: true,
+               stepSize: 1,
+               min: 0,
+               max: 5
+             }
+           }]
+         }
+      }
+    }
+  );
+
+  $('#content').css('width', '').show().siblings().hide();
+}
+
+function drawChart_2(input) {
+  console.log(input);
+
+  $('#content').append('<canvas id="myChart"></canvas>');
+  let ctx = $('#myChart').get(0).getContext('2d');
+  var myChart = new Chart(ctx,
+    {
+      type: 'bar',
+      data: {
+          labels: input.worker_name,
+          //group_worker order by worker_id
+          datasets: [{
+            label: "score~~~",
+            data: input.score,
+            backgroundColor: getRandomColor()
+          }]
+      },
+      options: {
+        scales: {
+           yAxes: [{
+             scaleLabel: {
+               display: true,
+               labelString: '評價分數'
+             },
+             ticks: {
+               beginAtZero: true,
+               stepSize: 1,
+               min: 0,
+               max: 5
+             }
+           }]
+         },
+      }
+    }
+  );
+
+  $('#content').css('width', '').show().siblings().hide();
+}
+
 function drawChart_4(data) {
   $('#content').append('<table id="table"></table>');
   $('#table').append('<tr><th>序號</th>><th>評分者UID</th><th>評分時間</th><th>滿意度</th><th>評分項目</th><th>被評分人員</th><th>團名</th></tr>');
   data.map(function(ele) {
-    console.log(ele);
+    // console.log(ele);
     $('#table').append('<tr><td>'+ele.ID+'</td>'
       + '<td>'+ele.vote_voter+'</td>'
       + '<td>'+ele.vote_time+'</td>'
@@ -45,4 +140,54 @@ function drawChart_4(data) {
   $('#content').css('width', 'auto').show().siblings().hide();
 }
 
-// function
+function drawChart_5(data) {
+  $('#content').append('<table id="category"></table>');
+  $('#category').append('<tr><th>序號</th>><th>項目名稱</th></tr>');
+  data.category.map(function(ele) {
+    // console.log(ele);
+    $('#category').append('<tr><td>'+ele.ID+'</td>'
+      + '<td>'+ele.category_name+'</td></tr>');
+  });
+
+  $('#content').append('<table id="worker"></table>');
+  $('#worker').append('<tr><th>序號</th>><th>名稱</th><th>項目</th><th>評價次數</th></tr>');
+  data.worker.map(function(ele) {
+    let category_name = data.category.filter( function(cate) {
+      return cate.ID ==  ele.category_id;
+    })[0].category_name;
+
+    $('#worker').append('<tr><td>'+ele.ID+'</td>'
+      + '<td>'+ele.worker_name+'</td>'
+      + '<td>'+category_name+'</td>'
+      + '<td>'+ele.vote_count+'</td></tr>');
+  });
+
+  $('#content').append('<table id="group"></table>');
+  $('#group').append('<tr><th>序號</th>><th>團體名稱</th><th>服務職稱</th></tr>');
+  data.group.map(function(ele) {
+    let group_worker = data.group_worker.filter( function(e) {
+      return e.group_id == ele.ID;
+    });
+    group_worker = group_worker.map( function(e) {
+      return data.worker.filter( function(worker) {
+        return worker.ID == e.worker_id;
+      })[0].worker_name;
+    });
+    let str = group_worker.join(',');
+
+    $('#group').append('<tr><td>'+ele.ID+'</td>'
+      + '<td>'+ele.group_name+'</td>'
+      + '<td>'+str+'</td></tr>');
+  });
+
+  $('#content').css('width', 'auto').show().siblings().hide();
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
--- routes/analyzeResponse.js
+++ routes/analyzeResponse.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -41,49 +40,253 @@ router.get('/', function(req, res, next) {
   });
 });
 router.post('/', function(req, res, next) {
-  console.log("get post! chart = "+req.body.chart);
-  let chart = req.body.chart;
-  let sql = req.body.sql;
-  if( chart=="某團各客戶對各工人評價" ) {
+  let updated = false;
 
-
-  }
-  else if( chart=="所有旅行團對該職業工人評價" ) {
-
-
-  }
-  else if( chart=="旅行團對各項目評價" ) {
-
-
-  }
-  else if( chart=="詳細資料" ) {
-    sql = "SELECT shield.vote.*, shield.category.category_name, shield.worker.worker_name, shield.group.group_name "
-     +" FROM shield.category, shield.worker, shield.group, shield.vote "
-     + " WHERE shield.vote.category_id = shield.category.ID "
-     + " and shield.vote.worker_id = shield.worker.ID "
-     + " and shield.vote.group_id = shield.group.ID";
-
-    con.query(sql, function(err, result, fields) {
-      if( err ) {
-        console.log("ERROR when SELECT * FROM shield.vote");
-        console.log(err);
+  //更新WORKER的評價資料
+  con.query('SELECT * FROM shield.update_info ORDER BY ID DESC LIMIT 1', function(err, result, fields) {
+    let update_to = result[0].update_to;      //取得上次更新VOTE資料庫到哪
+    con.query('SELECT * FROM shield.vote WHERE shield.vote.ID > ? ORDER BY worker_id', update_to,  function(err, result, fields) {
+      //取得上次更新VOTE資料庫之後，產生的新資料
+      if( result.length==0 ) {
+        //no need to update
+        console.log("no need update");  //若產生的新資料，長度=0，則不需更新
+        updated = true;
       }
       else {
-        for( let i in result ) {
-          delete result[i].category_id;
-          delete result[i].worker_id;
-          delete result[i].group_id;
+        console.log("need update");     //若有產生新資料，則須更新
+        let vote_data = [];             //將同樣WORKER_ID的VOTE_SCORE包一起，減少存取DB的動作
+
+        //在SELECT DB時，已經將DATA以WORKER_ID排序了
+        while( result.length>0 ) {      //只要還有待處理的VOTE資料
+          let worker_id = result[0].worker_id;  //取得現在要包的WORKER_ID
+          let scores = [];
+          while( result.length>0 ) {
+            if( result[0].worker_id == worker_id ) {    //只要這一筆資料還在同一個WORKER_ID
+              scores.push( result[0].vote_score );      //就把這個VOTE的分數PUSH進SCORES陣列
+              result.splice(0,1);                       //並把此VOTE移出待處理的陣列
+            }
+            else break;                     //如果不在同個WORKER_ID，就先跳出迴圈
+          }
+          vote_data.push({                  //PUSH一個物件，物件包含WORKER_ID及他獲得的評價分數們的陣列
+            worker_id: worker_id,
+            scores: scores
+          });
         }
-        res.send(result);
-      }
-    });
+        console.log(vote_data);
+
+        vote_data.map( function(data) {
+          con.query('SELECT * FROM shield.worker WHERE shield.worker.ID = ? ', data.worker_id, function(err, result, fields) {
+            let worker = result[0];   //獲得此WORKER之前在DB的資料
+            for( let i in data.scores ) {   //將前面獲得的VOTE_DATA裡面的每個SCORE，加進去此WORKER的資料
+              let n = data.scores[i];
+              worker["score_"+n+"_count"]++;
+              worker["vote_count"]++;
+            }
+            let sum = 0;
+            for( let i = 1; i<=5; i++ ) {   //加完SCORE後，計算出總共得分
+              sum += i * worker["score_"+i+"_count"];
+            }
+            worker.score = sum/worker.vote_count; //計算出平均得分
+            // console.log(worker);      //CONSOLE此WORKER更新後的資料
+            con.query('UPDATE shield.worker SET vote_count = ? '
+            + ', score_5_count = ?, score_4_count = ?, score_3_count = ?, score_2_count = ?, score_1_count = ?'
+            + ', score = ?'
+            + ' WHERE ID = ?'
+            , [worker.vote_count, worker.score_5_count, worker.score_4_count, worker.score_3_count, worker.score_2_count, worker.score_1_count, worker.score, data.worker_id]
+            , function(err, rows) {   //UPDATE進資料庫
+              if(err) throw err;
+            });
+          });
+        });
+
+        //紀錄這次更新到哪
+        con.query('SELECT ID FROM shield.vote ORDER BY ID DESC LIMIT 1', function(err, result, fields) {
+          //取得VOTE的最後一筆資料的ID
+          let new_update_to = result[0].ID;
+          console.log("update_to = "+new_update_to);
+          con.query('INSERT INTO shield.update_info SET ?', {update_to: new_update_to}, function(err, rows) {
+            if(err) throw err;  //將此資訊寫進DB，下次就知道要從這筆資料後更新
+          });
+          updated = true;
+        }); //end con.query
+      } //end else
+    }); //end con.query vote
+  }); //end con.query update_info
+
+  let timer = setInterval( function() {
+    //js異步處理特性，須用此方法，確認在DB更新完後，再載入資料
+    if( updated ) clearInterval(timer);
+    else {
+      console.log("update not done yet");
+      return;
+    }
+
+    console.log("get post! chart = "+req.body.chart);
+    let chart = req.body.chart;
+    let sql = req.body.sql;
+    if( chart=="某團各客戶對各工人評價" ) {
+      let group_id = req.body.option;
+      let count = 0;
+      let group_with_workers = [];
+      let users_votes = [];
 
 
-  }
-  else {
-    console.log("chart = "+chart+", not found!");
-  }
+      let sql = "SELECT shield.group_worker.worker_id, shield.worker.worker_name FROM shield.group_worker, shield.worker"
+        + " WHERE shield.group_worker.group_id = ? and shield.group_worker.worker_id = shield.worker.ID"
+        + " ORDER BY shield.group_worker.worker_id";
+      con.query(sql, group_id, function(err, result, fields) {
+        group_with_workers = result;
+        console.log(group_with_workers);
+        count++;
+      });
+      con.query("SELECT * FROM shield.vote WHERE group_id = ? ORDER BY vote_voter, worker_id", group_id, function(err, result, fields) {
+        users_votes = package(result, "vote_voter", "worker_id", "vote_score");
+        for( let i in users_votes ) console.log(users_votes[i]);
+        count++;
+      });
+
+
+      // con.query("SELECT * FROM shield.vote WHERE group_id = ? ORDER BY worker_id, vote_voter", group_id, function(err, result, fields) {
+      //
+      // });
+
+
+      let timer = setInterval(function() {
+        if( count==2 ) clearInterval(timer);
+        else return;
+        res.send({
+          group_with_workers: group_with_workers,
+          users_votes: users_votes
+        });
+      });
+
+    }
+
+    else if( chart=="所有旅行團對該職業工人評價" ) {
+      let category_id = req.body.option;
+      con.query('SELECT worker_name, score FROM shield.worker WHERE category_id = ?', category_id, function(err, result, fields) {
+        console.log(result);
+        let worker_name = result.map( function(ele) {
+          return ele.worker_name;
+        });
+        let score = result.map( function(ele) {
+          return ele.score;
+        });
+        res.send({
+          worker_name: worker_name,
+          score: score
+        });
+        console.log("sent");
+      });
+
+    }
+
+    else if( chart=="旅行團對各項目評價" ) {
+
+
+    }
+
+    else if( chart=="詳細資料" ) {
+      sql = "SELECT shield.vote.*, shield.category.category_name, shield.worker.worker_name, shield.group.group_name "
+       + " FROM shield.category, shield.worker, shield.group, shield.vote "
+       + " WHERE shield.vote.category_id = shield.category.ID "
+       + " and shield.vote.worker_id = shield.worker.ID "
+       + " and shield.vote.group_id = shield.group.ID";
+
+      con.query(sql, function(err, result, fields) {
+        if( err ) {
+          console.log("ERROR when SELECT * FROM shield.vote");
+          console.log(err);
+        }
+        else {
+          for( let i in result ) {
+            delete result[i].category_id;
+            delete result[i].worker_id;
+            delete result[i].group_id;
+          }
+          res.send(result);
+        }
+      });
+    }
+    else if( chart=="設定" ) {
+      let data = {};
+      let count = 0;
+      con.query("SELECT * FROM shield.category", function(err, result, fields) {
+        if( err ) console.log(err);
+        else {
+          data.category = result;
+          count++;
+        }
+      });
+      con.query("SELECT * FROM shield.worker", function(err, result, fields) {
+        if( err ) console.log(err);
+        else {
+          data.worker = result;
+          count++;
+        }
+      });
+      con.query("SELECT * FROM shield.group", function(err, result, fields) {
+        if( err ) console.log(err);
+        else {
+          data.group = result;
+          count++;
+        }
+      });
+      con.query("SELECT * FROM shield.group_worker", function(err, result, fields) {
+        if( err ) console.log(err);
+        else {
+          data.group_worker = result;
+          count++;
+        }
+      });
+      con.query("SELECT * FROM shield.vote", function(err, result, fields) {
+        if( err ) console.log(err);
+        else {
+          data.vote = result;
+          count++;
+        }
+      });
+      let timer = setInterval( function() {
+        if( count==5 ) clearInterval(timer);
+        else return;
+        res.send(data);
+      }, 10 );
+    }
+    else {
+      console.log("chart = "+chart+", not found!");
+    }
+
+  }, 2 ); //end updated timer
 });
 
+function package(arr, prop) {
+  console.log("prop = "+prop);
+  let return_data = [];
+  while( arr.length>0 ) {
+    let now_prop = arr[0][prop];
+    let data = [];
+    while( arr.length>0 ) {
+      if( arr[0][prop] == now_prop ) {
+        if( arguments.length==2 ) data.push( arr[0] );
+        else {
+          let obj = {};
+          for( let i=2; i<arguments.length; i++ ) {
+            let to_get = arguments[i];
+            obj[to_get] = arr[0][to_get];
+          }
+          data.push( obj );
+        }
+        arr.splice(0,1);                       //並把此VOTE移出待處理的陣列
+      }
+      else break;                     //如果不在同個WORKER_ID，就先跳出迴圈
+    }
+    let obj = {};
+    obj[prop] = now_prop;
+    obj["data"] = data;
+    return_data.push(obj);
+  }
+  // console.log(return_data);
+  return return_data;
+}
 
 module.exports = router;
